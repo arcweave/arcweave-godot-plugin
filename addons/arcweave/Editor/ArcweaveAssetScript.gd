6f3d4adabd2de443b9dab790afef3d7a9df3d5fe
Emmanouil Dermitzakis <manolis@arcweave.com>
2024-02-13 13:55:14 +0200

    chore: refactor asset inspector

--- addons/arcweave/Editor/ArcweaveAssetScript.gd
+++ addons/arcweave/Editor/ArcweaveAssetScript.gd
@@ -2,10 +2,22 @@
 class_name ArcweaveAsset extends Resource
 
 signal project_updated()
-@export var use_api : bool = false
+@export_group("File Settings")
+## The JSON file exported from Arcweave
+@export_global_file() var project_file : String 
+@export_group("API Settings")
+## The API Key of your Arcweave account
 @export var api_key : String
+## The Project Hash
 @export var project_hash : String
-@export var project_file : String
+
+@export_group("")
+## The retrieve method to use
+@export_enum("File", "WebAPI") var receive_method: String = "File" 
+@export var _placeholder : bool = false
+
+@export_category("Retrieved Settings")
+## The retrieved Project Settings
 @export var project_settings : Dictionary
 
 func _init(_project_settings = {}, _api_key = "", _project_hash = ""):
@@ -14,11 +26,11 @@ func _init(_project_settings = {}, _api_key = "", _project_hash = ""):
 	project_hash = _project_hash
 
 func refresh_project(api_request: APIRequest):
-	if use_api:
+	if receive_method == "WebAPI":
 		print("[Arcweave] Refreshing from API")
 		_refresh_from_api(api_request)
 		return
-	print("[Arcweave] Refreshing from file")	
+	print("[Arcweave] Refreshing from file")
 	_refresh_from_file()
 
 func _refresh_from_api(api_request: APIRequest):
@@ -37,4 +49,5 @@ func _refresh_from_file():
 	var data = file.get_as_text()
 	file.close()
 	project_settings = JSON.parse_string(data)
+	print("[Arcweave] Successfully refreshed from File!")
 	project_updated.emit(project_settings)
--- addons/arcweave/Editor/Inspector/ArcweaveInspector.gd
+++ addons/arcweave/Editor/Inspector/ArcweaveInspector.gd
@@ -1,35 +1,26 @@
 @tool
 extends EditorInspectorPlugin
 
-var handled_properties : PackedStringArray
 var api_request : APIRequest
-var ArcweavePropertyEditor = preload("res://addons/arcweave/Editor/Inspector/ArcweavePropertyEditor.gd")
 
-func _init():
-	handled_properties = PackedStringArray(["use_api", "project_file", "api_key", "project_hash"])
-
-# We are creating 
 func _can_handle(object):
 	return object is ArcweaveAsset
 
-func _parse_begin(object):
-	add_property_editor_for_multiple_properties("Arcweave Project Settings", handled_properties, ArcweavePropertyEditor.new())
-	var InitButton = Button.new()
-	api_request = APIRequest.new(object)
-	InitButton.add_child(api_request)
-	InitButton.add_theme_color_override("font_color", Color.DARK_ORANGE)
-	InitButton.text = "Initialize Arcweave Asset"
-	InitButton.pressed.connect(pressed.bind(object))
-	add_custom_control(InitButton)
-
 func _parse_property(object, type, name, hint_type, hint_string, usage_flags, wide):
-	if (handled_properties.has(name)):
+	if name == "_placeholder":
+		var InitButton = Button.new()
+		api_request = APIRequest.new(object)
+		InitButton.add_child(api_request)
+		InitButton.add_theme_color_override("font_color", Color.DARK_ORANGE)
+		InitButton.text = "Initialize Arcweave Asset"
+		InitButton.pressed.connect(pressed.bind(object))
+		add_custom_control(InitButton)
 		return true
 	return false
 
 func pressed(object: ArcweaveAsset):
-	object.refresh_project(api_request)
 	object.project_updated.connect(on_project_updated.bind(object), CONNECT_ONE_SHOT)
+	object.refresh_project(api_request)
 
 func on_project_updated(new_project_settings, object: ArcweaveAsset):
 	var error = ResourceSaver.save(object, object.resource_path)
